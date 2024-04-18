bf8c352bbec0e203da5d0781c28a91dee009d3b1
kmlproduct <kam.salaheddine@gmail.com>
2024-04-19 00:03:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [amountFrom, setAmountFrom] = useState("EUR");
+  const [amountTo, setAmountTo] = useState("USD");
+  const [outPut, setOutPut] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(
+    function () {
+      async function getCurrencyData() {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${amountFrom}&to=${amountTo}`
+        );
+        const data = await res.json();
+
+        setOutPut(data.rates[amountTo]);
+        setIsLoading(false);
+      }
+      if (amountFrom === amountTo) return setOutPut(amount);
+      getCurrencyData();
+    },
+    [amount, amountFrom, amountTo]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="box">
+        <input
+          type="text"
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+          disabled={isLoading}
+        />
+
+        <select
+          value={amountFrom}
+          onChange={(e) => setAmountFrom(e.target.value)}
+          disabled={isLoading}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <select
+          value={amountTo}
+          onChange={(e) => setAmountTo(e.target.value)}
+          disabled={isLoading}
+        >
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <p>
+          <span>{amountFrom}</span>= <span>{outPut} </span>
+          <span>{amountTo}</span>
+        </p>
+      </div>
     </div>
   );
 }
